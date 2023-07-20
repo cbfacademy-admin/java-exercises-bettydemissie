90d254c3f15da690137f7db08fb0ec83c6972b6b
Bethelham <betheldemis@yahoo.co.uk>
2023-07-20 21:22:33 +0100

    java collections academy exercise

--- exercises/java-collections/src/main/java/com/cbfacademy/CollectionsExercises.java
+++ exercises/java-collections/src/main/java/com/cbfacademy/CollectionsExercises.java
@@ -6,50 +6,96 @@ public class CollectionsExercises {
 
     public LinkedList<Integer> useLinkedList() {
         // TODO: create an empty linked list
-        //  - add 4 as the first element of the list
-        //  - then add 5, 6, 8, 2, 9 to the the list
-        //  - add another 2 as the last element of the list
-        //  - add 4 as the 3rd element of the list
-        //  - invoke the method element() on the list and print the result on the screen
-        //  - return the list
-        throw new RuntimeException("Not implemented");
+
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        // - add 4 as the first element of the list
+        linkedList.add(4);
+        // - then add 5, 6, 8, 2, 9 to the the list
+        linkedList.add(5);
+        linkedList.add(6);
+        linkedList.add(8);
+        linkedList.add(2);
+        linkedList.add(9);
+        
+        // - add another 2 as the last element of the list
+        linkedList.add(2);
+        // - add 4 as the 3rd element of the list
+        linkedList.add(2,4);
+        // - invoke the method element() on the list and print the result on the screen
+        System.out.print(linkedList.element());
+        // - return the list
+        return linkedList;
     }
 
     public Stack<Integer> useStack() {
         // TODO: create an empty stack
+
+        Stack<Integer> stackone = new Stack<>();
         //  - add 5, 6, 8, 9 to the the stack
+        stackone.add(5);
+        stackone.add(6);
+        stackone.add(8);
+        stackone.add(9);
+
         //  - print the first element of the stack on the screen
+        System.out.print(stackone.get(0));
         //  - print the last element of the stack on the screen
+        System.out.print(stackone.get(3));
         //  - invoke the method pop() on the stack and print the result on the screen
+        System.out.print(stackone.pop());
         //  - invoke the push(4) method on the stack
+        stackone.push(4);
         //  - return the stack
-        throw new RuntimeException("Not implemented");
+        return stackone;
     }
 
     public ArrayDeque<Integer> useArrayDeque() {
         // TODO: create an empty arrayDeque
-        //  - add 5, 6, 8, 9 to the the stack
+
+        ArrayDeque<Integer> array = new ArrayDeque<>();
+        // - add 5, 6, 8, 9 to the the stack
+        array.add(5);
+        array.add(6);
+        array.add(8);
+        array.add(9);
+
         //  - print the first element of the queue on the screen
+        System.out.print(array.peek());
         //  - print the last element of the queue on the screen
+        System.out.print(array.getLast());
         //  - invoke the method poll() on the queue and print the result on the screen
+        System.out.print(array.poll());
         //  - invoke the element() method on the queue and print the result on the screen
+        System.out.print(array.element());
         //  - return the queue
-        throw new RuntimeException("Not implemented");
+        return array;
     }
 
     public HashMap<Integer, String> useHashMap() {
         // TODO: create an empty hash map
-        //  - add {1, TypeScript} entry to the map
-        //  - add {2, Kotlin} entry to the map
-        //  - add {3, Python} entry to the map
-        //  - add {4, Java} entry to the map
-        //  - add {5, JavaScript} entry to the map
-        //  - add {6, Rust} entry to the map
+
+        HashMap<Integer, String> map = new HashMap<>();
+        // - add {1, TypeScript} entry to the map
+        map.put(1, "TypeScript");
+        // - add {2, Kotlin} entry to the map
+        map.put(2, "Kotlin");
+        // - add {3, Python} entry to the map
+        map.put(3, "Python");
+        // - add {4, Java} entry to the map
+        map.put(4, "Java");
+        // - add {5, JavaScript} entry to the map
+        map.put(5, "JavaScript");
+        // - add {6, Rust} entry to the map
+        map.put(6, "Rust");
+
         //  - determine the set of keys from the map and print it on the screen
+        System.out.print(map.keySet());
         //  - determine the set of keys from the map and print it on the screen
+        System.out.print(map.values());
         //  - determine whether the map contains "English" as a language and print the result on the screen
+        System.out.print(map.containsValue("English"));
         //  - return the map
-        throw new RuntimeException("Not implemented");
+        return map;
     }
 
     public String getName() {
